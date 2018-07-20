e9786d82b5cf3c2edb887d90add239a1a121c38c
Markus Scheidgen <markus.scheidgen@gmail.com>
2018-07-20 12:17:14 +0200

    Updated copyright info.

--- parser/parser-gamess/setup_paths.py
+++ parser/parser-gamess/setup_paths.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 Rosendo Valero, Fawzi Mohamed, Rosendo Valero Montero, Danio Brambila
+# Copyright 2016-2018 Rosendo Valero, Fawzi Mohamed, Danio Brambila
 # 
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
