d0636d38487307762b53a450d0bacbf2fcccf164
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2021-08-27 01:14:05 +0200

    Adapted further metainfo changes

--- gamessparser/metainfo/__init__.py
+++ gamessparser/metainfo/__init__.py
@@ -16,4 +16,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import gamessparser.metainfo.gamess
+from nomad.metainfo import Environment
+
+from . import gamess
+
+m_env = Environment()
+m_env.m_add_sub_section(Environment.packages, gamess.m_package)
--- gamessparser/metainfo/gamess.py
+++ gamessparser/metainfo/gamess.py
@@ -25,6 +25,9 @@ from nomad.metainfo import (  # pylint: disable=unused-import
 from nomad.datamodel.metainfo import run
 
 
+m_package = Package()
+
+
 class x_gamess_section_atom_forces(MSection):
     '''
     section that contains Cartesian forces of the system for a given geometry
